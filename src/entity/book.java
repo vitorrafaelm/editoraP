facbd75be13a8ac1eb1bcc7507e1005ab49ae911
mateus-lanuce <mateuslanuce@gmail.com>
2022-09-06 11:00:29 -0300

    create book class on entity

---
+++ src/entity/book.java
@@ -0,0 +1,17 @@
+package entity;
+
+class Book {
+
+    //private Author author;
+    //private Evaluator evaluator; 
+    private String title;
+    private String description;
+    private String gender;
+    private String year;
+    private String status_register; //aprovado, em analise, nao aprovado
+
+    
+
+
+
+}
