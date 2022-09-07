2ea2cec67cc9b16497440c0956d120f5f21402fe
Davi Clemente <davi.clemente@alunos.ufersa.edu.br>
2022-09-06 22:03:20 -0300

    Author criado
    Author criado e devidamente encapsulado

---
+++ src/entity/Author.java
@@ -0,0 +1,62 @@
+package entity;
+
+public class Author {
+	private String nome;
+	private String adress;
+	private String cpf;
+	
+	public Author(String nome, String adress, String cpf) {
+		setNome(nome);
+		setAdress(adress);
+		setCpf(cpf);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		if(!nome.isEmpty()) {
+			this.nome = nome;
+		}else {
+			System.out.println("Nome invalido");
+		}
+	}
+	public String getAdress() {
+		return adress;
+	}
+	
+	public void setAdress(String adress) {
+		if(!adress.isEmpty()) {
+			this.adress = adress;
+		}else { 
+			System.out.println("Endereço Invalido");
+		}
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		if(!cpf.isBlank()) {
+			this.cpf = cpf;
+		}else {
+			System.out.println("CPF invalido");
+		}
+	}
+
+	public void Cadastro (String status){
+		//cadastra um livro
+	}
+	
+	public void Atualizar (Author author) {
+		setNome(nome);
+		setAdress(adress);
+		setCpf(cpf);
+	}
+	public void Excluir (String status){
+		//exclui um livro já cadastrado
+	}
+	public String Login (String email) {
+		return "";
+	}
+}
