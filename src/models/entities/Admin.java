3ba55afc6cb752253f006e7a02bb405d3667edd9
nickolas-silva <nickolasemanuel10@gmail.com>
2022-10-13 14:07:12 -0300

    ajustando a classe admin

--- src/models/entities/Admin.java
+++ src/models/entities/Admin.java
@@ -1,36 +1,35 @@
 package models.entities;
 
 public class Admin {
-    private String name;
-    private String password;
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        long strcont = name.chars().filter(ch -> ch != ' ').count();
-        if(strcont > 4 && strcont < 30){
-            this.name = name;
-        }
-        
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        long cont = password.chars().filter(ch -> ch != ' ').count();
-        if(cont > 4 && cont < 13){
-            this.password = password;
-        }
-    }
-
-    public Admin(String name, String password){
-        setName(name);
-        setPassword(password);
-
-    }
-    
+	private int id;
+	private String name;
+	private String email;
+	private Address password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getPassword() {
+		return password;
+	}
+	public void setPassword(Address password) {
+		this.password = password;
+	}
+	
+	
 }
+
