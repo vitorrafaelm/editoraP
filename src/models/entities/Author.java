b258c01cdcb7f1c8b9285ad655d7e14f2cc3cda8
Davi Clemente <davi.clemente@alunos.ufersa.edu.br>
2022-10-13 23:48:44 -0300

    atualizei o author

--- src/models/entities/Author.java
+++ src/models/entities/Author.java
@@ -1,62 +1,91 @@
 package models.entities;
 
 public class Author {
-	private String nome;
-	private String adress;
-	private String cpf;
-	
-	public Author(String nome, String adress, String cpf) {
-		setNome(nome);
-		setAdress(adress);
-		setCpf(cpf);
-	}
+    private int id;
+    private String nome;
+    private String phone;
+    private Address address;
+    private String cpf;
+    
+    
+    public Author(String nome, String phone, Address address, String cpf) {
+        setNome(nome);
+        setPhone(phone);
+        setAddress(address);
+        setCpf(cpf);
+    }   
+        
+    public int getId() {
+        return this.id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		if(!nome.isEmpty()) {
-			this.nome = nome;
-		}else {
-			System.out.println("Nome invalido");
-		}
-	}
-	public String getAdress() {
-		return adress;
-	}
-	
-	public void setAdress(String adress) {
-		if(!adress.isEmpty()) {
-			this.adress = adress;
-		}else { 
-			System.out.println("Endere�o Invalido");
-		}
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		if(!cpf.isBlank()) {
-			this.cpf = cpf;
-		}else {
-			System.out.println("CPF invalido");
-		}
-	}
+    public void setId(int Id){
+        if(Id > 0){
+            this.id = Id;
+        }
+    }
 
-	public void Cadastro (String status){
-		//cadastra um livro
-	}
-	
-	public void Atualizar (Author author) {
-		setNome(nome);
-		setAdress(adress);
-		setCpf(cpf);
-	}
-	public void Excluir (String status){
-		//exclui um livro j� cadastrado
-	}
-	public String Login (String email) {
-		return "";
-	}
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        if(!phone.isEmpty()) { 
+            this.phone = phone;
+        }else {
+            System.out.println("Telefone invalido");
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public void setNome(String nome) {
+        if(!nome.isEmpty()) {
+            this.nome = nome;
+        }else {
+            System.out.println("Nome invalido");
+        }
+    }
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        if(address != null) {
+            this.address = address;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Author [name=" + nome + ", phone=" + phone + ", CPF=" + cpf + ", address=" + address + "]";
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        if(!cpf.isBlank()) {
+            this.cpf = cpf;
+        }else {
+            System.out.println("CPF invalido");
+        }
+    }
+
+    public void Cadastro (String status){
+        //cadastra um livro
+    }
+    
+    public void Atualizar (Author author) {
+        setNome(nome);
+        setAddress(address);
+        setCpf(cpf);
+    }
+    public void Excluir (String status){
+        //exclui um livro j  cadastrado
+    }
+    public String Login (String email) {
+        return "";
+    }
 }
