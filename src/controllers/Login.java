ce72c29a151b6fc72545dfc6cd9c27262d98ea75
vitorrafaelm <vitor.rafael1518@gmail.com>
2022-11-15 19:07:34 -0300

    feat: adding controllers to login screen

--- src/controllers/Login.java
+++ src/controllers/Login.java
@@ -1,17 +1,24 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import views.Telas;
 
 public class Login {
+    @FXML private TextField password;
+    @FXML private TextField cpf;
+    
+    @FXML private TextField cpfInRecoverPasswordScreen;
+    public String newPss;
     
     public void authenticateUser() {
-        
+        if(this.password.getText().isEmpty() == false && this.cpf.getText().isEmpty() == false) {
+            Telas.telaHomePage();
+        }
          // Pegar password e user
          // Tentar autenticar
          // Caso de erro exibir mensagem para o usuário
-         // Se der certo redirecionar ele para a home screen
-        
-        Telas.telaHomePage();
+         // Se der certo redirecionar ele para a home screen  
     }
     
     public void goToLoginScreen() {
@@ -23,8 +30,10 @@ public class Login {
     }
     
     public void generateNewPasswordAndNavigate() {
-        String newpassword = "123456";
+        if(cpfInRecoverPasswordScreen.getText().isEmpty() == false) {
+            this.newPss = "123456";
+            Telas.PasswordInformationScreen();
+        }
         
-        Telas.PasswordInformationScreen();
     }
 }
