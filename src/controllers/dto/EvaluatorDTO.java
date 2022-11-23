e985da84ff28d9316a70c0670c21a4f6d11a5c45
Davi Clemente <davi.clemente@alunos.ufersa.edu.br>
2022-11-23 16:04:12 -0300

    Update EvaluatorDTO.java

--- src/controllers/dto/EvaluatorDTO.java
+++ src/controllers/dto/EvaluatorDTO.java
@@ -4,38 +4,57 @@ import models.entities.Address;
 
 public class EvaluatorDTO {
     private int id;
+    private String name, cpf, email, senha;
+    private Address address;
+   
+    
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    private String name, cpf;
-    private Address address;
-    
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getCpf() {
         return cpf;
     }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public String getEmail() {
+        return email;
     }
-
+    public String getSenha() {
+        return senha;
+    }
     public Address getAddress() {
         return address;
     }
+    
+    public void setId(int id) {
+       if (id > 0) {
+          this.id = id;
+       }
+    }
 
-    public void setAddress(AddressDTO address) {
-        //this.address = address;
+    public void setName(String name) {
+       if (name != null && !name.isEmpty()) {
+           this.name = name;
+       }
+    }
+
+    public void setCpf(String cpf) {
+        if(cpf != null && !cpf.isEmpty()){
+            this.cpf = cpf;
+        }
+    }
+    public void setEmail(String email) {
+        if (email != null && !email.isEmpty()) {
+            this.email = email;
+        }
+     }
+    public void setSenha(String senha) {
+        if (senha != null && !senha.isEmpty()) {
+            this.senha = senha;
+        }
+     }
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
